68bfce5ac4ae1fe94910e361f439c0b654c01cbe
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-05-10 23:52:21 +0200

    constructor

--- Program.cs
+++ Program.cs
@@ -38,14 +38,20 @@ class Program
         }*/
         Console.WriteLine();
 
-        //Object
-        Robot bot = new Robot();
-        bot.setValues("Healer", 800, new byte[]{0,0,0});
-        bot.printValues();
+        //Objects
+        Robot healer = new Robot("Healer", 800, new byte[]{0,10,0});
+        //healer.setValues("Healer", 800, new byte[]{0,10,0});
+        healer.printValues();
         
         Robot killer = new Robot();
         killer.setValues("Killer", 1000, new byte[] {0,0,10});
         killer.printValues();
+
+        Robot bot = new Robot("Bot");
+        bot.printValues();
+
+        Robot bot1 = new Robot("Bot1");
+        bot1.printValues();
     
 
 
--- Robot.cs
+++ Robot.cs
@@ -2,9 +2,22 @@ using System;
 
 namespace CS_exercise1{
   class Robot{
-    private string name;
-    private int weight;
-    private byte[] coordinates;
+    private string name="Bot";
+    private int weight=100;
+    private byte[] coordinates={0,0,0};
+
+    public Robot(string _name, int _weight, byte[] _coordinates){
+      setValues(_name, _weight, _coordinates);
+      Console.WriteLine("Object has been created");
+    }
+    public Robot(string _name){
+      name = _name;
+      Console.WriteLine("Object has been created");
+    }
+
+    public Robot(){
+      Console.WriteLine("Object has been created");
+    }
 
     public void setValues(string _name, int _weight, byte[] _coordinates){
       name = _name;
