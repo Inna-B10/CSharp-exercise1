59f20d2a7cb5d963dda4dd140b263a7ad0fa70db
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-05-10 22:02:50 +0200

    creating variables, array and list

--- Program.cs
+++ Program.cs
@@ -1,9 +1,29 @@
-﻿namespace CS_exercise1;
+﻿using System;
+using System.Collections.Generic;
+
+namespace CS_exercise1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int a = 16;
+        double x = 5.324d;
+        string level = "user";
+        string[] day = {"Monday", "Wednesday", "Friday"};
+        List<string> AuthorList = new List<string>();
+        AuthorList.Add("Dickens");
+        AuthorList.Add("Christie");
+        AuthorList.Add("Tolkien");
+        AuthorList.Add("Orwell");
+        for(int i=0; i<day.Length; i++){
+            Console.WriteLine(day[i]);
+        }
+        for(int i=0; i<AuthorList.Count; i++){
+            Console.WriteLine(AuthorList[i]);
+        }
+        /*foreach(string author in AuthorList){
+            Console.WriteLine(author);
+        }*/
     }
 }
